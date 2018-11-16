c3c2bd9d02be301f0d8055b353faf5604d895d54
Mariusz <you@example.com>
2018-11-16 17:09:58 +0100

    add check empty articles list

--- src/pl/sda/CountBill.java
+++ src/pl/sda/CountBill.java
@@ -21,15 +21,20 @@ public class CountBill {
         BigDecimal totalCost;
         double shipingCostInZl = shipingCost * dolarCost / articles.size();
 
-        for (int n = 0; articles.size() > n; n++) {
-            System.out.println("-----------------------------------------------------------------------------------");
-            shipingCostOneProduct = shipingCostInZl / articles.get(n).getNumber();
-            totalCost = BigDecimal.valueOf(articles.get(n).getCost()).multiply((BigDecimal.valueOf(dolarCost)))
-                    .add(BigDecimal.valueOf(shipingCostOneProduct));
-            totalCost = totalCost.add(totalCost.multiply(BigDecimal.valueOf(tax / 100)));
-            System.out.printf((n + 1) + ". Nazwa towaru: " + articles.get(n).getName() + ", ilość sztuk : "
-                    + articles.get(n).getNumber() + ", cena netto za 1 szt: %2.2f zł", totalCost);
-            System.out.println();
+        if(articles.isEmpty()){
+
+            System.out.println("Nie dodałeś żadnego artykułu !!!!");
+        }else{
+            for (int n = 0; articles.size() > n; n++) {
+                System.out.println("-----------------------------------------------------------------------------------");
+                shipingCostOneProduct = shipingCostInZl / articles.get(n).getNumber();
+                totalCost = BigDecimal.valueOf(articles.get(n).getCost()).multiply((BigDecimal.valueOf(dolarCost)))
+                        .add(BigDecimal.valueOf(shipingCostOneProduct));
+                totalCost = totalCost.add(totalCost.multiply(BigDecimal.valueOf(tax / 100)));
+                System.out.printf((n + 1) + ". Nazwa towaru: " + articles.get(n).getName() + ", ilość sztuk : "
+                        + articles.get(n).getNumber() + ", cena netto za 1 szt: %2.2f zł", totalCost);
+                System.out.println();
+            }
         }
     }
 
@@ -42,10 +47,8 @@ public class CountBill {
             number = sc.nextInt();
             System.out.println("Podaj cene w $: ");
             cost = sc.nextDouble();
-
             articles.add(new Articles(name, number, cost));
             sc.nextLine();
-
             System.out.println("wprowadzmy jeszcze t/n");
             dec = sc.nextLine();
         } while ((!dec.equals("n")));
@@ -54,16 +57,23 @@ public class CountBill {
     }
 
     public void showList(List<Articles> articles) {
-        for (Articles a : articles) {
-            i++;
-            System.out.println(i + ", Nazwa: " + a.getName() + ", Ilosc szt: " + a.getNumber() +
-                    ", Cena za 1 szt.: " + a.getCost() + " $");
+
+        if (articles.isEmpty()) {
+
+            System.out.println("Nie dodałeś jeszcze artykułów !!!!");
+
+        } else {
+            for (Articles a : articles) {
+                i++;
+                System.out.println(i + ", Nazwa: " + a.getName() + ", Ilosc szt: " + a.getNumber() +
+                        ", Cena za 1 szt.: " + a.getCost() + " $");
+            }
+            i = 0;
         }
-        i = 0;
     }
+
 
     public List<Articles> getArticles() {
         return articles;
     }
-
 }
